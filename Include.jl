38b4c6b9d9013ed019fb6ad3a80fe4ebc80bc6a3
Jeffrey Varner <jdv27@cornell.edu>
2025-11-21 12:57:13 -0500

    Add initial project files and synthetic data notebook
    Added Include.jl for environment setup, PS6-CHEME-5660-Solution-SyntheticDataGen-Fall-2025.ipynb notebook for synthetic data generation, Project.toml for Julia dependencies, and multiple data files required for financial modeling and analysis.

---
+++ Include.jl
@@ -0,0 +1,29 @@
+# setup paths -
+const _ROOT = @__DIR__;
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+const _PATH_TO_DATA = joinpath(_ROOT, "data");
+const _PATH_TO_FIGS = joinpath(_ROOT, "figs");
+
+# make sure all is up to date -
+using Pkg
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# load external packages -
+using VLQuantitativeFinancePackage
+using DataFrames
+using Statistics
+using StatsBase
+using Plots
+using Colors
+using PrettyTables
+using CSV
+using Distributions
+using FileIO
+using JLD2
+using HypothesisTests
+using StatsPlots
+using LinearAlgebra
+using Test
